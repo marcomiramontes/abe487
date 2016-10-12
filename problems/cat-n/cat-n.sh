9c5909d7e7a5d6b0d8c8022ebf7e176900a2d51e
marcomiramontes <marcomiramontes@email.arizona.edu>
2016-10-12 13:47:17 -0700

    new homework

--- problems/cat-n/cat-n.sh
+++ problems/cat-n/cat-n.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
+set -u
+
+if [[ $# -lt 1 ]]; then
+	printf "Usage: cat-n.sh file"
+	exit 1
+
+elif [[ $# -gt 0 ]]; then
+	file=$1
+	i=1
+	while read -r line;
+		do
+		echo $i $line
+		let i++
+		done < $file	
+
+fi
+
+
 echo "OK"
